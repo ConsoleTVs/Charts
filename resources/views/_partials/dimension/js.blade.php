d4a3d597b638e6172515e351bd7735571cbc30fd
Kovah <mail@kovah.de>
2016-11-02 10:26:22 +0100

    Allow the user to set a height for responsive charts

--- resources/views/_partials/dimension/js.blade.php
+++ resources/views/_partials/dimension/js.blade.php
@@ -1,6 +1,8 @@
-@if($model->responsive)
-    height: 100%,
+@if($model->responsive && $model->height)
+    height: {{ $model->height }},
     width: 100%,
+@elseif($model->responsive)
+    height="100%" width="100%"
 @else
     @if($model->height)
         height: {{ $model->height }},
